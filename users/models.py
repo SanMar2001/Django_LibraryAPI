d5e029c4204aa3aae444f17ceaed968a73177990
SanMar2001 <s.martinez4@utp.edu.co>
2024-04-03 09:13:24 -0500

    Usuarios corregidos

--- users/models.py
+++ users/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Admin(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    dni = models.CharField(max_length=30, blank=False)
+    dni = models.CharField(max_length=30, blank=False, unique=True)
     names = models.CharField(max_length=80, blank=False)
     surnames = models.CharField(max_length=80, blank=False)
     birthdate = models.DateField(null=False)
-    birthplace = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
-    gender = models.CharField(max_length=30)
+    birthplace = models.CharField(max_length=30, null=False)
+    address = models.CharField(max_length=100, null=False)
+    gender = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return f"{self.names} {self.surnames}"
@@ -20,9 +20,9 @@ class Client(models.Model):
     names = models.CharField(max_length=80, blank=False)
     surnames = models.CharField(max_length=80, blank=False)
     birthdate = models.DateField(null=False)
-    birthplace = models.CharField(max_length=30)
-    address = models.CharField(max_length=100)
-    gender = models.CharField(max_length=30)
+    birthplace = models.CharField(max_length=30, null=False)
+    address = models.CharField(max_length=100, null=False)
+    gender = models.CharField(max_length=30, null=False)
     fav_topics = models.TextField(blank=True)
 
     def __str__(self):
